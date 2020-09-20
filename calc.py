e38b9f11a6dab67ee34648771ff35f31d7af3128
unknown <bbshj9531@naver.com>
2020-09-20 22:09:08 +0900

    update calc.py

--- calc.py
+++ calc.py
@@ -2,8 +2,8 @@
 def sum_function( n1 , n2):  # add two number
     return n1 + n2
 
-num1 = int ( input(“ input number 1” ) )
-num2 = int ( input(“ input number 2” ) )
+num1 = int ( input(“ input number 1” ))
+num2 = int ( input(“ input number 2” ))
 
 sum = sum_function( num1 , num2)
 
